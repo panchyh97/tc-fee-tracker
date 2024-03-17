4159af33189b99e990e927b0aa4cc48496f7a69a
Francisca Hernández <f.hernandezp@outlook.com>
2024-03-16 23:00:10 -0300

    style: :lipstick: add bg svg

--- src/app/(drawerContent)/layout.tsx
+++ src/app/(drawerContent)/layout.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from '@/components';
 
 export default function Layout({ children }: { children: React.ReactNode; }) {
     return (
-        <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+        <div className="overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
             <div className="flex">
                 <Sidebar />
                 <div className="p-2 w-full text-slate-900">
